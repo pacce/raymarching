c82f9652169828d5936800c93f43533b9b8f14be
Pedro Henrique Dalla Pacce <pedrodallapacce@gmail.com>
2024-03-09 22:20:31 -0300

    refactor: removes libav

---
+++ include/raymarching/raymarching.hpp
@@ -0,0 +1,7 @@
+#ifndef RAYMARCHING_HPP__
+#define RAYMARCHING_HPP__
+
+namespace raymarching {
+} // namespace raymarching
+
+#endif // RAYMARCHING_HPP__
--- source/main.cpp
+++ source/main.cpp
@@ -1,13 +1,4 @@
 #include <iostream>
-extern "C" {
-    #include <libavcodec/avcodec.h>
-    #include <libavformat/avformat.h>
-    #include <libavutil/frame.h>
-    #include <libavutil/imgutils.h>
-    #include <libavutil/opt.h>
-    #include <libswscale/swscale.h>
-}
-
 
 int
 main(int, char**) {
